9617ab083eb55e3dee0d222ffe71709a29678877
ZeroByter <romgamesrom@gmail.com>
2024-03-16 22:36:45 +0200

    fixed problem with colorpicker visibility

--- colorpicker-canvas.js
+++ colorpicker-canvas.js
@@ -26,29 +26,30 @@ canvasParent.addEventListener("mousemove", e => {
   ctx.clearRect(0, 0, canvas.width, canvas.height)
 
   ctx.fillStyle = "rgba(0,0,0,0.75)"
+  const yOffset = Math.min(Math.max(0, mouseY - pickerSize / 2), canvas.height - pickerSize)
   if (mouseX < canvas.width / 2) {
-    ctx.drawImage(mouseAreaColorsData, canvas.width - pickerSize, 0)
-    ctx.fillRect(canvas.width - pickerSize / 2 - 2, pickerSize / 2 - 2, 4, 4)
+    ctx.drawImage(mouseAreaColorsData, canvas.width - pickerSize, yOffset)
+    ctx.fillRect(canvas.width - pickerSize / 2 - 2, yOffset + pickerSize / 2 - 2, 4, 4)
   } else {
-    ctx.drawImage(mouseAreaColorsData, 0, 0)
-    ctx.fillRect(pickerSize / 2 - 2, pickerSize / 2 - 2, 4, 4)
+    ctx.drawImage(mouseAreaColorsData, 0, yOffset)
+    ctx.fillRect(pickerSize / 2 - 2, yOffset + pickerSize / 2 - 2, 4, 4)
   }
 })
 
 const onTouchMove = e => {
   if (e.touches.length > 0) {
-    const rect = canvasParent.getBoundingClientRect()
     const [mouseX, mouseY] = elementLocalToGlobal(canvasParent, e.touches[0].clientX, e.touches[0].clientY)
 
     ctx.clearRect(0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = "rgba(0,0,0,0.75)"
+    const yOffset = Math.min(Math.max(0, mouseY - pickerSize / 2), canvas.height - pickerSize)
     if (mouseX < canvas.width / 2) {
-      ctx.drawImage(mouseAreaColorsData, canvas.width - pickerSize, 0)
-      ctx.fillRect(canvas.width - pickerSize / 2 - 2, pickerSize / 2 - 2, 4, 4)
+      ctx.drawImage(mouseAreaColorsData, canvas.width - pickerSize, yOffset)
+      ctx.fillRect(canvas.width - pickerSize / 2 - 2, yOffset + pickerSize / 2 - 2, 4, 4)
     } else {
-      ctx.drawImage(mouseAreaColorsData, 0, 0)
-      ctx.fillRect(pickerSize / 2 - 2, pickerSize / 2 - 2, 4, 4)
+      ctx.drawImage(mouseAreaColorsData, 0, yOffset)
+      ctx.fillRect(pickerSize / 2 - 2, yOffset + pickerSize / 2 - 2, 4, 4)
     }
   }
 }
